a8dff5b9fca51e186aacdf5fe3027f6c7956bf5b
Raphaëlddddddddddddddd Heng <raphael.heng@epita.fr>
2022-04-06 21:57:22 +0200

    save

--- php/head.php
+++ php/head.php
@@ -6,3 +6,5 @@
 <!-- links-->
 <link rel="styleSheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"> <!--Symbols-->
 <link rel="stylesheet" href="../css/style.css"> <!--Css file-->
+<link rel="stylesheet" href="../css/footer.css">
+<link rel="stylesheet" href="../css/header.css">
