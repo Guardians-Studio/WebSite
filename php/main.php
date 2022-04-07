9047cab8e1eac336abdb663dc9b815cd450b8819
Raphaëlddddddddddddddd Heng <raphael.heng@epita.fr>
2022-04-07 20:35:13 +0200

    modif summary

--- php/main.php
+++ php/main.php
@@ -12,20 +12,32 @@
         </head>
 
         <section class="summary">
-            <section class="content">
-                <p id="titleSummary">Sommaire :</p>
-                <ul>
-                    <li><a href="game.php">Le Jeu</a></li>
-                    <li><a href="download.php">Télechargements</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                    <br> <br>
-                </ul>
-            </section>
+            <h2>Présentation brève du Guardians Studio : </h2>
+            <p>
+                Nous sommes un groupe d'étudiants de première année de cycle préparatoire de l'école EPITA.
+            </p>
+
+            <p>
+                Tout a commencé pendant nos révisions de partiels de mi-semestre 1. <br>
+                Après avoir travaillé ensemble, nous avons remarqué que nous nous entendions bien et que nous étions efficaces dans les révisions. <br>
+                C'est donc ainsi que les prémices du Guardians Studio ont vu le jour.
+            </p>
+
+            <p>
+                Las de la tendance des jeux à être purement compétitifs, nous nous sommes donnés l'objectif de publier un jeu coopératif pour encourager les joueurs à s'amuser ensemble plutôt que de s'affronter. Ainsi naquit Era Of Guardians: Timeless Village
+            </p>
+
+            <h2>Présentation brève de Era Of Guardians: Timeless Village :</h2>
+            <p>
+                Era Of Guardians: Timeless Village est un RPG qui se joue seul ou en coopération. L'équipe de joueur est amenée, à travers une aventure scénarisée, à évoluer dans différents biomes pour récupérer 4 gemmes et combattre le Gardien du Temps afin de rétablir le cours du temps qui s'est interrompu dans le village d'Hazeltown.
+            </p>
+
+            <p>Nous vous invitons à naviguer sur les différentes pages du site pour avoir plus de détails.</p>
         </section>
 
         <section class="game">
             <balise id="game"><h3>Le jeu</h3></balise>
-                <p>Voici quelques aperçues de notre jeu
+                <p>Voici quelques aperçus de notre jeu
         </section>
 
         <section class="l'équipe">
