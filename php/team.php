14383e9125e9858e0c6dfebfcecf94e09dda309a
Raphaëlddddddddddddddd Heng <raphael.heng@epita.fr>
2022-04-07 20:47:52 +0200

    modif team page

--- php/team.php
+++ php/team.php
@@ -14,13 +14,13 @@
             <figure class="member reveal">
                 <img src="../img/alexandre" alt="alexandre">
                 <figcaption>
-                    Alexandre Privat : Insérer description
+                    Alexandre Privat : Fan de casses-tête et de réflexion, j'ai découvert la programmation en classe de première, ma curiosité de découvrir différents langages informatiques et de découvrir la composition d'une machine m'a motivé à me lancer dans une branche de l'ingénierie informatique.
                 </figcaption>
             </figure>
 
             <figure class="member reveal">
                 <figcaption>
-                    Erwann Lesech : Insérer description
+                    Erwann Lesech : Roux et motard du groupe, sportif originaire du sud, j'ai découvert le développement informatique il y a 4 ans. J'ai été amené à coder différents petits jeux et applications sans grandes ambitions.
                 </figcaption>
                 <img src="../img/erwann" alt="erwann">
             </figure>
@@ -28,13 +28,13 @@
             <figure class="member reveal">
                 <img src="../img/guillaume" alt="guillaume">
                 <figcaption>
-                    Guillaume Jolivalt : Insérer description
+                    Guillaume Jolivalt : Curieux, je m'intéresse à beaucoup de domaines de la programmation et de l'informatique depuis bien longtemps, je n'ai malgré cela encore jamais entrepris de projet aussi conséquent que celui-ci.
                 </figcaption>
             </figure>
 
             <figure class="member reveal">
                 <figcaption>
-                    Raphaël Heng : Insérer description
+                    Raphaël Heng : Amateur de montage vidéo du groupe, j'ai commencé à programmer dès l'année de Seconde avec l'enseignement d'exploration Informatique et Création Numérique. J'y ai découvert des bases de Python, de HTML et de CSS.
                 </figcaption>
                 <img src="../img/raphael" alt="raphael">
             </figure>
