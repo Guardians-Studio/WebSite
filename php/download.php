40388dd61a02a5a415e666b732c38ac5b77b8a95
Raphaëlddddddddddddddd Heng <raphael.heng@epita.fr>
2022-04-08 23:35:41 +0200

    save

--- php/download.php
+++ php/download.php
@@ -7,24 +7,28 @@
 
     <body>
         <?php include("header.php"); ?>
-        <section id="reports-links-section">
+        <section>
             <h1>Documents officiels : </h1>
 
-            <a href="../downloads/cahier_des_charges.pdf" class="download-link" download>Cahier des charges</a>
+            <ul class="links-list" id="reports-links-section">
+                <li><a href="../downloads/cahier_des_charges.pdf" class="download-link" download>Cahier des charges</a></li>
 
-            <a href="../downloads/rapport_soutenance1.pdf" class="download-link" download>Rapport de soutenance 1</a>
+                <li><a href="../downloads/rapport_soutenance1.pdf" class="download-link" download>Rapport de soutenance 1</a></li>
 
-            <a href="../downloads/rapport_soutenance2.pdf" class="download-link" download>Rapport de soutenance 2</a>
+                <li><a href="../downloads/rapport_soutenance2.pdf" class="download-link" download>Rapport de soutenance 2</a></li>
 
-            <a href="../downloads/rapport_projet.pdf" class="download-link" download>Rapport de projet</a>
+                <li><a href="../downloads/rapport_projet.pdf" class="download-link" download>Rapport de projet</a></li>
+            </ul>
         </section> 
 
-        <section id="launcher-link-section">
+        <section>
             <h1>Launcher et documentation : </h1>
 
-            <a href="../downloads/launcher.exe" class="download-link" download>Télécharger le jeu</a>
-
-            <a href="../downloads/manual.pdf" class="download-link" download>Télécharger le manuel</a>
+            <ul class="links-list" id="launcher-link-section">
+                <li><<a href="../downloads/launcher.exe" class="download-link" download>Télécharger le jeu</a></li>
+                <li><a href="../downloads/manual.pdf" class="download-link" download>Télécharger le manuel</a></li>
+            </ul>
+            
         </section>
 
         <?php include("footer.php"); ?>
