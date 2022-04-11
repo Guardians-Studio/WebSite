cc66a4c86ecd6a0c80295f209412043b66b020a1
Raphaël Heng <raphael.heng@epita.fr>
2022-04-11 15:54:26 +0200

    just saving

--- send_email.php
+++ send_email.php
@@ -6,9 +6,14 @@ if (isset($_POST['submit']))
     $subject = "Mail de formulaire";
     $name = $_POST['name'];
     $message = $name . ' ' . $from . ' a écrit ' . "\n\n" . $_POST['message'];  
-
-    $headers = 'From: ' . $name . ' ' . $from . ' ' . "\n\n";
+    $headers = 'From: ' . $name . ' ' . $from . ' ' . "\n\n";    
     mail($to,$subject,$message,$headers);
+
+    $headers_back = 'From: Guardians Studio \n\n';
+    $message_to_user = 'Merci ' . $name . ' pour votre message.';
+    $subject_user = "Confirmation d'envoi de mail";
+    mail($from,$subject_user,$message_to_user,$headers_back);
+
     header('Location: https://guardiansstudio.000webhostapp.com/contact.php');
     exit();
 }
