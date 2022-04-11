135e12e7b23e722845759ccb356baef17c7497b1
Raphaël Heng <raphael.heng@epita.fr>
2022-04-11 17:19:05 +0200

    bark

--- index.php
+++ index.php
@@ -48,27 +48,9 @@
         <section class="contact">
             <balise id="Nous Contacter"><h3>Nous Contacter</h3></balise>
             <p>
-                Pour nous contacter :
+                <a href="contact.php">Pour nous contacter :</a>
             </p>
-            <form method="POST" action="send_email.php">
-                <p>
-                    <label for="name">Nom :</label>
-                    <input type="text" name="name">
-                </p>
-                <p>
-                    <label for="email">Email : </label>
-                    <input type="text" name="email">
-                </p>
-                <p>
-                    <label for="message">Message :</label>
-                    <br>
-                    <textarea name="message" cols="50" rows="20"></textarea>
-                    <br>
-                </p>
-                <p>
-                    <input type="submit" name="submit" value="Envoyer le message">
-                </p>
-            </form>
+            
         </section>
         
         <?php include("footer.php"); ?>
