017657e4ceace9d6ad5bd01ce197114fdb6126a8
Raphaël Heng <raphael.heng@epita.fr>
2022-04-11 16:05:45 +0200

    save

--- send_email.php
+++ send_email.php
@@ -7,12 +7,14 @@ if (isset($_POST['submit']))
     $name = $_POST['name'];
     $message = $name . ' ' . $from . ' a écrit ' . "\n\n" . $_POST['message'];  
     $headers = 'From: ' . $name . ' ' . $from . ' ' . "\n\n";    
-    mail($to,$subject,$message,$headers);
+    mail($to, $subject, $message, $headers);
 
-    $headers_back = 'From: Guardians Studio \n\n';
-    $message_to_user = 'Merci ' . $name . ' pour votre message.';
-    $subject_user = "Confirmation d'envoi de mail";
-    mail($from,$subject_user,$message_to_user,$headers_back);
+    $name_to_user = "Guardians Studio";
+    $email_to_user = "no-reply@guardians-studio.com";
+    $headers_back = 'From: ' . $name_to_user . ' ' . $email_to_user . ' ' . "\n\n";
+    $message_to_user = 'Merci ' . $name . ' pour votre message. Voici une copie' . "\n\n" . $message;
+    $subject_to_user = "Confirmation d'envoi de mail";
+    mail($from, $subject_to_user, $message_to_user, $headers_back);
 
     header('Location: https://guardiansstudio.000webhostapp.com/contact.php');
     exit();
