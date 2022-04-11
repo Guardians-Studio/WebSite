1cef9e25835114b9de92936d3b6e9f51c41b4c19
Raphaël Heng <raphael.heng@epita.fr>
2022-04-11 16:21:17 +0200

    save

--- send_email.php
+++ send_email.php
@@ -5,13 +5,15 @@ if (isset($_POST['submit']))
     $from = $_POST['email'];
     $subject = "Mail de formulaire";
     $name = $_POST['name'];
-    $message = $name . ' ' . $from . ' a écrit ' . "\n\n" . $_POST['message'];  
-    $headers = 'From: ' . $name . ' ' . $from . ' ' . "\n\n";    
+    $message = $name . ' ' . $from . ' a écrit '  . "\n\n" . $_POST['message'];  
+    $headers = 'From: ' . $name . ' ' . $from . ' ' . "\n\n";
+    
     mail($to, $subject, $message, $headers);
 
     $name_to_user = "Guardians Studio";
     $email_to_user = "no-reply@guardians-studio.com";
     $headers_back = 'From: ' . $name_to_user . ' ' . $email_to_user . ' ' . "\n\n";
+    
     $message_to_user = 'Merci ' . $name . ' pour votre message. Voici une copie' . "\n\n" . $message;
     $subject_to_user = "Confirmation d'envoi de mail";
     mail($from, $subject_to_user, $message_to_user, $headers_back);
