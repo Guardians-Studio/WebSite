5209a705945b74a8cd3ea9e0c368cda403220086
Raphaël Heng <raphael.heng@epita.fr>
2022-05-11 16:41:10 +0200

    save

--- download.php
+++ download.php
@@ -12,11 +12,11 @@
 
             <h1 class="installation-title">Documents officiels : </h1>
             <ul class="links-lists" id="reports-links-section">
-                <li><a href="downloads/cahier_des_charges.pdf" class="download-link" target="_blank">Cahier des charges</a></li>
+                <li><a href="downloads/cahier_des_charges.pdf" class="download-link" download>Cahier des charges</a></li>
 
-                <li><a href="downloads/rapport_soutenance1.pdf" class="download-link" target="_blank">Rapport de soutenance 1</a></li>
+                <li><a href="downloads/rapport_soutenance1.pdf" class="download-link" download>Rapport de soutenance 1</a></li>
 
-                <li><a href="downloads/rapport_soutenance2.pdf" class="download-link" target="_blank">Rapport de soutenance 2</a></li>
+                <li><a href="downloads/rapport_soutenance2.pdf" class="download-link" download>Rapport de soutenance 2</a></li>
                 <!-- 
                 <li><a href="downloads/rapport_projet.pdf" class="download-link" target="_blank">Rapport de projet</a></li>
                  -->
@@ -27,8 +27,8 @@
             <h1 class="installation-title">Installateur et manuels : </h1>
             <ul class="links-lists" id="launcher-link-section">
                 <li><a href="https://drive.google.com/uc?export=download&id=1qmoIDrFgYGA0LQYhrMdKv7M5m0gKVgqM" class="download-link" target="_blank">Télécharger le jeu</a></li>
-                <li><a href="downloads/installation_manual.pdf" class="download-link" target="_blank">Télécharger le manuel d'installation</a></li>
-                <li><a href="downloads/use_manual.pdf" class="download-link" target="_blank">Télécharger le manuel d'utilisation</a></li>
+                <li><a href="downloads/installation_manual.pdf" class="download-link" download>Télécharger le manuel d'installation</a></li>
+                <li><a href="downloads/use_manual.pdf" class="download-link" download>Télécharger le manuel d'utilisation</a></li>
             </ul>
         </section>
         <?php include("footer.php"); ?>
