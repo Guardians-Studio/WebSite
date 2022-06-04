08f7a684f5ac0064b1c4128cdee4973e9f276631
Raphaël Heng <raphael.heng@epita.fr>
2022-06-04 11:55:04 +0200

    updating website

--- download.php
+++ download.php
@@ -17,9 +17,9 @@
                 <li><a href="downloads/rapport_soutenance1.pdf" class="download-link" download>Rapport de soutenance 1</a></li>
 
                 <li><a href="downloads/rapport_soutenance2.pdf" class="download-link" download>Rapport de soutenance 2</a></li>
-                <!-- 
+                 
                 <li><a href="downloads/rapport_projet.pdf" class="download-link" download>Rapport de projet</a></li>
-                 -->
+                 
             </ul>
         </section> 
 
@@ -27,7 +27,7 @@
             <h1 class="installation-title">Installateur et manuels : </h1>
             <ul class="links-lists" id="launcher-link-section">
                 <li><a href="downloads/Era_Of_Guardians_TV_Launcher.zip" class="download-link" download>Télécharger le jeu (Launcher Portable)</a></li>
-                <li><a href="#" class="download-link">Télécharger le jeu (Setup)</a></li>
+                <li><a href="https://drive.google.com/file/d/1n2eg6ODRBjklFzdbAl1DLwYZGu0ca02a/view?usp=sharing" class="download-link">Télécharger le jeu (Setup)</a></li>
                 <li><a href="downloads/installation_manual.pdf" class="download-link" download>Télécharger le manuel d'installation</a></li>
                 <li><a href="downloads/use_manual.pdf" class="download-link" download>Télécharger le manuel d'utilisation</a></li>
             </ul>
